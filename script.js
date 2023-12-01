41ead46e5fd0c156390312f13a7f4d7062ea70cf
chill31 <89910163+chill31@users.noreply.github.com>
2023-12-01 14:46:53 +0530

    more error handling and languages percentage also added

--- script.js
+++ script.js
@@ -1,5 +1,55 @@
-const repoList = document.querySelector(".repository-list");
+const colorsMap = {
+  "JavaScript": "f1e05a",
+  "HTML": "e34c26",
+  "CSS": "563d7c",
+  "Python": "3572a5",
+  "C++": "f34b7d",
+  "C": "555555",
+  "Shell": "89e051",
+  "TypeScript": "2b7489",
+  "Ruby": "701516",
+  "Java": "b07219",
+  "Go": "00add8",
+  "PHP": "4f5d95",
+  "C#": "178600",
+  "Jupyter Notebook": "da5b0b",
+  "Objective-C": "438eff",
+  "Swift": "ffac45",
+  "Rust": "dea584",
+  "Scala": "c22d40",
+  "Dart": "00b4ab",
+  "Kotlin": "f18e33",
+  "Vue": "2c3e50",
+  "Clojure": "db5855",
+  "Lua": "000080",
+  "Assembly": "6E4C13",
+  "Perl": "0298c3",
+  "CoffeeScript": "244776",
+  "Emacs Lisp": "c065db",
+  "Elixir": "6e4a7e",
+  "Haskell": "5e5086",
+  "Dockerfile": "384d54",
+  "PowerShell": "012456",
+  "R": "198ce7",
+  "Makefile": "427819",
+  "TeX": "3d6117",
+  "Vue": "2c3e50",
+  "Clojure": "db5855",
+  "Lua": "000080",
+  "Assembly": "6E4C13",
+  "Perl": "0298c3",
+  "CoffeeScript": "244776",
+  "Emacs Lisp": "c065db",
+  "Elixir": "6e4a7e",
+  "Haskell": "5e5086",
+  "Dockerfile": "384d54",
+  "PowerShell": "012456",
+  "R": "198ce7",
+  "Makefile": "427819",
+  "TeX": "3d6117"
+};
 
+const repoList = document.querySelector(".repository-list");
 
 const cacheTimestamp = localStorage.getItem("repoCacheTimestamp");
 const currentTimestamp = new Date().getTime();
@@ -7,18 +57,14 @@ const cacheAgeInDays = (currentTimestamp - cacheTimestamp) / (1000 * 60 * 60 * 2
 
 if (cacheTimestamp && cacheAgeInDays < 15) {
   const cachedData = JSON.parse(localStorage.getItem("repoCache"));
-  console.log(cachedData);
-  displayRepos(cachedData);
+  const cachedLanguages = JSON.parse(localStorage.getItem("repoLanguagesCache"));
+  displayRepos(cachedData, cachedLanguages);
 } else {
   fetch("https://api.github.com/users/chill31/repos")
     .then(response => response.json())
     .then(data => {
       if (data.message === 'Not Found') {
-        console.error("Error fetching data from GitHub");
-        const errorSpan = document.createElement('span');
-        errorSpan.classList.add('error-span');
-        errorSpan.textContent = 'Error fetching data from GitHub. Try reloading this page.';
-        repoList.appendChild(errorSpan);
+        showError();
         return;
       }
 
@@ -34,27 +80,43 @@ if (cacheTimestamp && cacheAgeInDays < 15) {
           homepage: repo.homepage,
           language: repo.language,
           topics: repo.topics,
-          default_branch: repo.default_branch
+          default_branch: repo.default_branch,
+          languages: repo.languages_url
         };
         return filteredRepo;
       });
-      // Cache the data
+
+      filteredData.sort((a, b) => {
+        return new Date(b.created_at) - new Date(a.created_at);
+      });
+
+      let languages = [];
+
+      const languagesPromises = filteredData.map(repo => {
+        return fetch(repo.languages).then(response => response.json()).then(l => {
+
+          languages.push(l);
+
+        }).catch(error => {
+          console.log(error);
+          showError();
+        });
+      });
+
       localStorage.setItem("repoCache", JSON.stringify(filteredData));
+      localStorage.setItem("repoLanguagesCache", JSON.stringify(languages));
       localStorage.setItem("repoCacheTimestamp", currentTimestamp);
 
-      displayRepos(data);
+      displayRepos(filteredData);
     })
     .catch(error => {
-      console.error("Error fetching data from GitHub:", error);
-      const errorSpan = document.createElement('span');
-      errorSpan.classList.add('error-span');
-      errorSpan.textContent = 'Error fetching data from GitHub. Try reloading this page.';
-      repoList.appendChild(errorSpan);
+      console.log(error);
+      showError();
     });
 }
 
-function displayRepos(repos) {
-  repos.forEach(repo => {
+function displayRepos(repos, languages) {
+  repos.forEach((repo, index) => {
     const repoEl = document.createElement("div");
     repoEl.classList.add("repository");
 
@@ -63,10 +125,49 @@ function displayRepos(repos) {
       truncatedTitle = truncatedTitle.substring(0, 20) + "...";
     }
 
+    const languagesString = languages[index] ? Object.keys(languages[index]).map(language => {
+
+      const percentage =
+      Math
+      .round((languages[index][language] / Object.values(languages[index])
+      .reduce((a, b) => a + b, 0)) * 100);
+
+      return `<span class="language" style="background-color: #${colorsMap[language]}; width: ${percentage}%; height: 1rem;"></span>`;
+
+    }).join('') : '';
+
+    const languagesKeys = languages[index] ? Object.keys(languages[index]).map(language => {
+        
+        const percentage =
+        Math
+        .round((languages[index][language] / Object.values(languages[index])
+        .reduce((a, b) => a + b, 0)) * 100);
+  
+        const html =
+`<span class="language-key">
+  <span class="lang-bullet" style="background-color: #${colorsMap[language]}"></span>
+  <span class="lang-name">${language}</span>
+  <span class="lang-percentage">${percentage}%</span>
+</span>`;
+
+        return html;
+    }).join('') : '';
+
     repoEl.innerHTML =
-`<h2 class="repo-title">${truncatedTitle}</h2>
+      `<h2 class="repo-title">${truncatedTitle}</h2>
 ${truncatedTitle.length === repo.name.length ? '' : ""}
 ${repo.description ? `<p class="repo-description">${repo.description}</p>` : '<p class="no-repo-description">No description provided.</p>'}
+
+<div class="repo-languages">${languagesString}</div>
+<div class="repo-languages-keys">${languagesKeys}</div>
+
+<br>
+<br>
+
+<div class="repo-topics">
+  <strong>Topics:</strong>
+  ${repo.topics.length === 0 ? `<span class="no-topics-span">No topics</span>` : repo.topics.map(topic => `<span class="topic">#${topic}</span>`).join('')}
+</div>
 
 <span class="divider"></span>
 
@@ -102,7 +203,7 @@ ${repo.description ? `<p class="repo-description">${repo.description}</p>` : '<p
   <span class="info-span"><strong>Website</strong>
   <span class="span-data">
     <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M352 256c0 22.2-1.2 43.6-3.3 64H163.3c-2.2-20.4-3.3-41.8-3.3-64s1.2-43.6 3.3-64H348.7c2.2 20.4 3.3 41.8 3.3 64zm28.8-64H503.9c5.3 20.5 8.1 41.9 8.1 64s-2.8 43.5-8.1 64H380.8c2.1-20.6 3.2-42 3.2-64s-1.1-43.4-3.2-64zm112.6-32H376.7c-10-63.9-29.8-117.4-55.3-151.6c78.3 20.7 142 77.5 171.9 151.6zm-149.1 0H167.7c6.1-36.4 15.5-68.6 27-94.7c10.5-23.6 22.2-40.7 33.5-51.5C239.4 3.2 248.7 0 256 0s16.6 3.2 27.8 13.8c11.3 10.8 23 27.9 33.5 51.5c11.6 26 20.9 58.2 27 94.7zm-209 0H18.6C48.6 85.9 112.2 29.1 190.6 8.4C165.1 42.6 145.3 96.1 135.3 160zM8.1 192H131.2c-2.1 20.6-3.2 42-3.2 64s1.1 43.4 3.2 64H8.1C2.8 299.5 0 278.1 0 256s2.8-43.5 8.1-64zM194.7 446.6c-11.6-26-20.9-58.2-27-94.6H344.3c-6.1 36.4-15.5 68.6-27 94.6c-10.5 23.6-22.2 40.7-33.5 51.5C272.6 508.8 263.3 512 256 512s-16.6-3.2-27.8-13.8c-11.3-10.8-23-27.9-33.5-51.5zM135.3 352c10 63.9 29.8 117.4 55.3 151.6C112.2 482.9 48.6 426.1 18.6 352H135.3zm358.1 0c-30 74.1-93.6 130.9-171.9 151.6c25.5-34.2 45.2-87.7 55.3-151.6H493.4z"/></svg>
-    <a href="${repo.html_url}">${repo.homepage}</a>
+    <a href="${repo.homepage}">${repo.homepage}</a>
   </span>
 </span>
 
@@ -111,4 +212,17 @@ ${repo.description ? `<p class="repo-description">${repo.description}</p>` : '<p
 
     repoList.appendChild(repoEl);
   });
+}
+
+function showError() {
+  console.error("Error fetching data from GitHub");
+  const errorSpan = document.createElement('span');
+  errorSpan.classList.add('error-span');
+  errorSpan.textContent = 'Error fetching data from GitHub. Try reloading this page.';
+  repoList.appendChild(errorSpan);
+
+  // reset the local storage cache
+  localStorage.removeItem("repoCache");
+  localStorage.removeItem("repoLanguagesCache");
+  localStorage.removeItem("repoCacheTimestamp");
 }
