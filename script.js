9db6d395cc05eab309ed58f92f6ed230f258824e
chill31 <89910163+chill31@users.noreply.github.com>
2023-12-02 11:12:27 +0530

    fixed languages error and overflow in languages

--- script.js
+++ script.js
@@ -96,10 +96,10 @@ if (cacheTimestamp && cacheAgeInDays < 15) {
 
       const fetched = await fetchLanguages(filteredData)
       localStorage.setItem("repoCache", JSON.stringify(filteredData));
-      localStorage.setItem("repoLanguagesCache", JSON.stringify(languages));
+      localStorage.setItem("repoLanguagesCache", JSON.stringify(fetched));
       localStorage.setItem("repoCacheTimestamp", currentTimestamp);
 
-      displayRepos(filteredData, languages);
+      displayRepos(filteredData, fetched);
 
     })
     .catch(error => {
@@ -173,7 +173,7 @@ ${repo.description ? `<p class="repo-description">${repo.description}</p>` : '<p
     </span>
   </span>
   <span class="info-span">
-  <strong>Created At</strong>
+  <strong>Main Branch</strong>
   <span class="span-data">
     <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><path d="M80 104a24 24 0 1 0 0-48 24 24 0 1 0 0 48zm80-24c0 32.8-19.7 61-48 73.3v87.8c18.8-10.9 40.7-17.1 64-17.1h96c35.3 0 64-28.7 64-64v-6.7C307.7 141 288 112.8 288 80c0-44.2 35.8-80 80-80s80 35.8 80 80c0 32.8-19.7 61-48 73.3V160c0 70.7-57.3 128-128 128H176c-35.3 0-64 28.7-64 64v6.7c28.3 12.3 48 40.5 48 73.3c0 44.2-35.8 80-80 80s-80-35.8-80-80c0-32.8 19.7-61 48-73.3V352 153.3C19.7 141 0 112.8 0 80C0 35.8 35.8 0 80 0s80 35.8 80 80zm232 0a24 24 0 1 0 -48 0 24 24 0 1 0 48 0zM80 456a24 24 0 1 0 0-48 24 24 0 1 0 0 48z"/></svg>
     ${repo.default_branch}
