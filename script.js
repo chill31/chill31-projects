f85c4745a0b903234193d869120f63d6f863aba5
chill31 <89910163+chill31@users.noreply.github.com>
2024-07-11 23:22:31 +0530

    removed clutter

--- script.js
+++ script.js
@@ -82,12 +82,9 @@ if (cacheTimestamp && cacheAgeInDays < 15) {
           name: repo.name,
           full_name: repo.full_name,
           html_url: repo.html_url,
-          description: repo.description,
-          created_at: repo.created_at,
           homepage: repo.homepage,
           language: repo.language,
           topics: repo.topics,
-          default_branch: repo.default_branch,
           languages: repo.languages_url
         };
         return filteredRepo;
@@ -141,7 +138,7 @@ function displayRepos(repos, languages) {
       percentage = Math.round(percentage * 100) / 100;
 
       const html =
-        `<span class="language-key" style="transition-delay: ${200*(langIndex+1)}ms">
+        `<span class="language-key" style="transition-delay: ${200 * (langIndex + 1)}ms">
   <span class="lang-bullet" style="background-color: #${colorsMap[language]}"></span>
   <span class="lang-name">${language}</span>
   <span class="lang-percentage">${percentage}%</span>
@@ -150,63 +147,28 @@ function displayRepos(repos, languages) {
       return html;
     }).join('') : '';
 
+    const sourceButton = document.createElement('button');
+    sourceButton.classList.add('repo-source-button');
+    sourceButton.innerHTML = 
+`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+</svg>
+Source`;
+
+    sourceButton.addEventListener('click', () => goToGitHub(repo.html_url));
+
     repoEl.innerHTML =
-      `<h2 class="repo-title">${truncatedTitle}</h2>
-${truncatedTitle.length === repo.name.length ? '' : ""}
-${repo.description ? `<p class="repo-description">${repo.description}</p>` : '<p class="no-repo-description">No description provided.</p>'}
+      `<a href="${repo.homepage}"><h2 class="repo-title">${truncatedTitle}</h2></a>
+
+<div class="repo-topics">${repo.topics.length === 0 ? `` : repo.topics.map(topic => `<span class="topic">#${topic}</span>`).join('')}</div>
 
 <div class="repo-languages">${languagesString}</div>
 <div class="repo-languages-keys">${languagesKeys}</div>
-
-<br>
-<br>
-
-<div class="repo-topics">
-  <strong>Topics:</strong>
-  ${repo.topics.length === 0 ? `<span class="no-topics-span">No topics</span>` : repo.topics.map(topic => `<span class="topic">#${topic}</span>`).join('')}
-</div>
-
-<span class="divider"></span>
-
-<div class="repo-info">
-
-  <span class="info-span">
-    <strong>Created At</strong>
-    <span class="span-data">
-      <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/></svg>
-      ${new Date(repo.created_at).toLocaleString()}
-    </span>
-  </span>
-  <span class="info-span">
-  <strong>Main Branch</strong>
-  <span class="span-data">
-    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><path d="M80 104a24 24 0 1 0 0-48 24 24 0 1 0 0 48zm80-24c0 32.8-19.7 61-48 73.3v87.8c18.8-10.9 40.7-17.1 64-17.1h96c35.3 0 64-28.7 64-64v-6.7C307.7 141 288 112.8 288 80c0-44.2 35.8-80 80-80s80 35.8 80 80c0 32.8-19.7 61-48 73.3V160c0 70.7-57.3 128-128 128H176c-35.3 0-64 28.7-64 64v6.7c28.3 12.3 48 40.5 48 73.3c0 44.2-35.8 80-80 80s-80-35.8-80-80c0-32.8 19.7-61 48-73.3V352 153.3C19.7 141 0 112.8 0 80C0 35.8 35.8 0 80 0s80 35.8 80 80zm232 0a24 24 0 1 0 -48 0 24 24 0 1 0 48 0zM80 456a24 24 0 1 0 0-48 24 24 0 1 0 0 48z"/></svg>
-    ${repo.default_branch}
-  </span>
-</span>
-  <span class="info-span"><strong>Most Common Language</strong>
-    <span class="span-data">
-      <svg xmlns="http://www.w3.org/2000/svg" height="16" width="20" viewBox="0 0 640 512"><path d="M392.8 1.2c-17-4.9-34.7 5-39.6 22l-128 448c-4.9 17 5 34.7 22 39.6s34.7-5 39.6-22l128-448c4.9-17-5-34.7-22-39.6zm80.6 120.1c-12.5 12.5-12.5 32.8 0 45.3L562.7 256l-89.4 89.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-112-112c-12.5-12.5-32.8-12.5-45.3 0zm-306.7 0c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l112 112c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256l89.4-89.4c12.5-12.5 12.5-32.8 0-45.3z"/></svg>
-      ${repo.language}
-    </span>
-  </span>
-  <span class="info-span"><strong>Github</strong>
-    <span class="span-data">
-      <svg xmlns="http://www.w3.org/2000/svg" height="16" width="15.5" viewBox="0 0 496 512"><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/></svg>
-      <a href="${repo.html_url}">${repo.html_url}</a>
-    </span>
-  </span>
-
-  <span class="info-span"><strong>Website</strong>
-  <span class="span-data">
-    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M352 256c0 22.2-1.2 43.6-3.3 64H163.3c-2.2-20.4-3.3-41.8-3.3-64s1.2-43.6 3.3-64H348.7c2.2 20.4 3.3 41.8 3.3 64zm28.8-64H503.9c5.3 20.5 8.1 41.9 8.1 64s-2.8 43.5-8.1 64H380.8c2.1-20.6 3.2-42 3.2-64s-1.1-43.4-3.2-64zm112.6-32H376.7c-10-63.9-29.8-117.4-55.3-151.6c78.3 20.7 142 77.5 171.9 151.6zm-149.1 0H167.7c6.1-36.4 15.5-68.6 27-94.7c10.5-23.6 22.2-40.7 33.5-51.5C239.4 3.2 248.7 0 256 0s16.6 3.2 27.8 13.8c11.3 10.8 23 27.9 33.5 51.5c11.6 26 20.9 58.2 27 94.7zm-209 0H18.6C48.6 85.9 112.2 29.1 190.6 8.4C165.1 42.6 145.3 96.1 135.3 160zM8.1 192H131.2c-2.1 20.6-3.2 42-3.2 64s1.1 43.4 3.2 64H8.1C2.8 299.5 0 278.1 0 256s2.8-43.5 8.1-64zM194.7 446.6c-11.6-26-20.9-58.2-27-94.6H344.3c-6.1 36.4-15.5 68.6-27 94.6c-10.5 23.6-22.2 40.7-33.5 51.5C272.6 508.8 263.3 512 256 512s-16.6-3.2-27.8-13.8c-11.3-10.8-23-27.9-33.5-51.5zM135.3 352c10 63.9 29.8 117.4 55.3 151.6C112.2 482.9 48.6 426.1 18.6 352H135.3zm358.1 0c-30 74.1-93.6 130.9-171.9 151.6c25.5-34.2 45.2-87.7 55.3-151.6H493.4z"/></svg>
-    <a href="${repo.homepage}">${repo.homepage}</a>
-  </span>
-</span>
-
 </div>
 `;
 
+
+    repoEl.appendChild(sourceButton);
     repoList.appendChild(repoEl);
   });
 
@@ -254,6 +216,10 @@ async function fetchLanguages(filteredData) {
   return languages;
 }
 
+function goToGitHub(link) {
+  window.location.href = link;
+}
+
 function showFetchMessages() {
   const randomFetchMessages = [
     "Be patient...",
@@ -276,5 +242,5 @@ function showFetchMessages() {
   setTimeout(() => {
     clearInterval(msgInterval);
     fetchMessageSpan.textContent = "This is taking longer than expected...";
-  }, 5000);
+  }, 6000);
 }
